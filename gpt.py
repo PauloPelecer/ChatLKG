00516d23f1edbc0c530d759ff7df2dfd2d43d7c7
SenhorLoock <71995271+SenhorLoock@users.noreply.github.com>
2023-06-27 22:20:28 -0300

    Atualizar o gpt.py

--- gpt.py
+++ gpt.py
@@ -5,8 +5,69 @@ import os
 import re
 # Configuração da autenticação
 openai.api_key = Keys.KeyIa()
+msg = None
+def ShowList():
+    files = os.listdir('Documentos')
+    if files == []:
+        Digitar('Nenhum Arquivo Salvo!')
+        return files
+    else:
+        Digitar('Arquivos Salvos:\n')
+        for file in files:
+            file += '\n'
+            for c in file:
+                print (c, end='', flush=True)
+                time.sleep(0.05)
+        
 
 
+def Open():
+    l = ShowList()
+    if l == []:
+        pass
+    else:
+        Digitar('Qual Arquivo Deseja Abrir?')
+        file = input(str('\n\033[0;32mNome do Arquivo\033[0;33m:\033[0;m'))
+        with open(os.path.join('Documentos', file), 'r') as filedata:
+            data = filedata.read()
+            filedata.close()
+        IdePy(data)
+
+
+    
+def IdePy(text):
+        resposta = re.sub(r'import ','\033[0;34mimport \033[0;m',text) 
+        resposta = re.sub(r'from ', '\033[0;34mfrom \033[0;m', resposta) 
+        resposta = re.sub(r'global ', '\033[0;34mglobal \033[0;m', resposta) 
+  
+        resposta = re.sub(r' try', '\033[0;34m try\033[0;m',resposta) 
+        resposta = re.sub(r' except', '\033[0;34m except\033[0;m',resposta) 
+        resposta = re.sub(r' True', '\033[0;32m True\033[0;m',resposta) 
+        resposta = re.sub(r' as ', '\033[0;34m as \033[0;m',resposta) 
+        resposta = re.sub(r' False', '\033[0;31m False\033[0;m',resposta) 
+        resposta = re.sub(r'print', '\033[0;34mprint\033[0;m',resposta) 
+        resposta = re.sub(r'elif', '\033[0;34melif\033[0;m',resposta) 
+        resposta = re.sub(r'else', '\033[0;34melse\033[0;m',resposta) 
+        resposta = re.sub(r'if', '\033[0;34mif\033[0;m',resposta) 
+        resposta = re.sub(r'for ', '\033[0;34mfor \033[0;m',resposta) 
+        resposta = re.sub(r' in ', '\033[0;34m in \033[0;m',resposta) 
+        resposta = re.sub(r'while', '\033[0;34mwhile\033[0;m',resposta) 
+        resposta = re.sub(r'"', '\033[0;33m"\033[0;m',resposta) 
+        resposta = re.sub(r"'", "\033[0;33m'\033[0;m",resposta) 
+        resposta = re.sub(r'\(', '\033[0;34m(\033[0;m',resposta) 
+        resposta = re.sub(r'\)', '\033[0;34m)\033[0;m',resposta) 
+        resposta = re.sub(r'#', '\033[0;35m#\033[0;m',resposta) 
+        Digitar(resposta)
+        return resposta
+def Remove():
+    try:
+        ShowList()
+        Digitar('Qual Arquivo Deseja Apagar?')
+        filename = input(str('\n\033[0;32mNome Do Arquivo\033[0;33m:\033[0;m'))
+        os.remove(os.path.join('Documentos', filename))
+        Digitar('Arquivo Apagado!')
+    except OSError:
+        Digitar('Aquivo Não Encontrado!')
 
 def Container():
     if os.path.exists('Documentos'):
@@ -14,30 +75,30 @@ def Container():
         qnt = 0
         for file in files:
             qnt += 1
-        if qnt == None:
-            return 0
-        else:
-            return qnt
+        return qnt
     else:
         os.mkdir('Documentos')
         Container()
 
 def Save(msg,filename):
-    doc = 'Documentos'
-    home = os.path.join(doc, filename)
-    if os.path.exists(doc):
-        with open(home, 'w') as file:
-            file.write(msg[0])
-            file.close()
-            return "Arquivo Salvo!"
-    else:
-        print ('Criando Pasta ',doc,' Aguarde Alguns Segundos!')
-        time.sleep(2)
-        os.mkdir(doc)
-        with open(home, 'w') as file:
-            file.write(msg[0])
-            file.close()
-            return "Arquivo Salvo!"
+    try:
+        doc = 'Documentos'
+        home = os.path.join(doc, filename)
+        if os.path.exists(doc):
+            with open(home, 'w') as file:
+                file.write(msg[0])
+                file.close()
+                return "Arquivo Salvo!"
+        else:
+            print ('Criando Pasta ',doc,' Aguarde Alguns Segundos!')
+            time.sleep(2)
+            os.mkdir(doc)
+            with open(home, 'w') as file:
+                file.write(msg[0])
+                file.close()
+                return "Arquivo Salvo!"
+    except TypeError:
+        return 'Não a Nada Para Salvar!'
 
     
 def Main():
@@ -48,6 +109,9 @@ def Main():
 \033[0;34mComandos\033[0;m: \033[0;32m|---------------------\033[0;33m+\033[0;m
  \033[0;33m/\033[0;34mSalvar  \033[0;32m|\033[0;34mPergunte Oque Quiser\033[0;33m!\033[0;32m|
  \033[0;33m/\033[0;34mLimpar  \033[0;32m|---------------------\033[0;33m+
+ \033[0;33m/\033[0;34mArquivos\033[0;32m|
+ \033[0;33m/\033[0;34mRemover \033[0;32m|
+ \033[0;33m/\033[0;34mAbrir   \033[0;32m|
  \033[0;33m/\033[0;34mSair    \033[0;32m|
 \033[0;32m----------\033[0;33m+\033[0;m''')
     qnt = Container()
@@ -69,7 +133,7 @@ def Exiting():
 
 
 def Digitar(text):
-    print ('\033[0;33mGPT\033[0;31m:\033[0;m ')
+    print ('\n\033[0;33mGPT\033[0;31m:\033[0;m ')
     for c in text:
         print (c, end='', flush=True)
         time.sleep(0.05)
@@ -86,36 +150,14 @@ def obter_resposta(mensagem):
     )
 
     Onemsg = [response.choices[0].message.content, response.usage]
-    resposta = Onemsg[0]
-    resposta = re.sub(r'import ','\033[0;34mimport \033[0;m',resposta)
-    resposta = re.sub(r'from ', '\033[0;34mfrom \033[0;m', resposta)
-    resposta = re.sub(r'global ', '\033[0;34mglobal \033[0;m', resposta)
-
-    resposta = re.sub(r' try', '\033[0;34m try\033[0;m',resposta)
-    resposta = re.sub(r' except', '\033[0;34m except\033[0;m',resposta)
-    resposta = re.sub(r' True', '\033[0;32m True\033[0;m',resposta)
-    resposta = re.sub(r' as ', '\033[0;34m as \033[0;m',resposta)
-    resposta = re.sub(r' False', '\033[0;31m False\033[0;m',resposta)
-    resposta = re.sub(r'print', '\033[0;34mprint\033[0;m',resposta)
-    resposta = re.sub(r'elif', '\033[0;34melif\033[0;m',resposta)
-    resposta = re.sub(r'else', '\033[0;34melse\033[0;m',resposta)
-    resposta = re.sub(r'if', '\033[0;34mif\033[0;m',resposta)
-    resposta = re.sub(r'for ', '\033[0;34mfor \033[0;m',resposta)
-    resposta = re.sub(r' in ', '\033[0;34m in \033[0;m',resposta)
-    resposta = re.sub(r'while', '\033[0;34mwhile\033[0;m',resposta)
-    resposta = re.sub(r'"', '\033[0;33m"\033[0;m',resposta)
-    resposta = re.sub(r"'", "\033[0;33m'\033[0;m",resposta)
-    resposta = re.sub(r'\(', '\033[0;34m(\033[0;m',resposta)
-    resposta = re.sub(r'\)', '\033[0;34m)\033[0;m',resposta)
-    resposta = re.sub(r'#', '\033[0;35m#\033[0;m',resposta)
-    Digitar(resposta)
+    IdePy(Onemsg[0])
     return Onemsg
 
-
+text = None
 Main()
 while True:
     try:
-        global text
+        
         duvida = input(str('\n\033[0;32mUsuario\033[33m:\033[m '))
         if duvida == '/sair' or duvida == '/Sair':
             Exiting()
@@ -126,6 +168,12 @@ while True:
             Digitar(msg)
         elif duvida == '/Limpar' or duvida == '/limpar':
             Main()
+        elif duvida == '/Arquivos' or duvida == '/arquivos':
+            ShowList()
+        elif duvida == '/Remover' or duvida == '/remover':
+            Remove()
+        elif duvida == '/Abrir' or duvida == '/abrir':
+            Open()
         else:
             quest = [{'role':'system','content':'Voce e um assistente gente boa.'}]
             quest.append({'role':'user','content': str(duvida)})
